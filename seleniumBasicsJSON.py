ca5e41c036567bc14e9226953e58452199078933
Tyler Durette <td@my-mac-mini.attlocal.net>
2024-01-29 04:00:21 -0500

    adding timestamp and saving data to .json file

--- seleniumBasicsJSON.py
+++ seleniumBasicsJSON.py
@@ -1,11 +1,17 @@
 import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 import time
 import json
+import datetime
 
-# Initialize the Chrome WebDriver
-driver = webdriver.Chrome()
+# Configure Chrome options
+chrome_options = Options()
+chrome_options.add_argument("--headless")  # Run Chrome in headless mode
+
+# Initialize the Chrome WebDriver with the options
+driver = webdriver.Chrome(options=chrome_options)
 
 # Navigate to the URL
 url = 'https://data.vsin.com/linetracker/?sportid=cbb'
@@ -26,14 +32,17 @@ rows = tbody.find_elements(By.TAG_NAME, "tr")
 for row in rows:
     cells = row.find_elements(By.TAG_NAME, "td")
     cell_data = [cell.text for cell in cells]
-    # Here we assume each row has the same number of cells and there are no headers provided.
-    # If there are headers, they should be extracted similarly and used as keys for the dictionaries.
     row_data = {'Column{}'.format(index+1): value for index, value in enumerate(cell_data)}
     data.append(row_data)
 
 # Close the WebDriver
 driver.quit()
 
-# Write the data to a JSON file
-with open('output_data.json', 'w', encoding='utf-8') as json_file:
+# Generate a timestamp
+current_time = datetime.datetime.now()
+timestamp = current_time.strftime("%Y%m%d_%H%M%S")
+
+# Write the data to a JSON file with timestamp in the filename
+filename = f'output_data_{timestamp}.json'
+with open(filename, 'w', encoding='utf-8') as json_file:
     json.dump(data, json_file, ensure_ascii=False, indent=4)
